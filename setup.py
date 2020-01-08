1ef3e0267fd0b83c8f45bc076a1d4c57e9f2f696
Jim Martens <github@2martens.de>
2020-01-08 21:01:02 +0100

    Bumped required python version number

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     entry_points={
         "console_scripts": ["tm-allrisscraper = twomartens.allrisscraper.main:main"]
     },
-    python_requires="~=3.6",
+    python_requires="~=3.7",
     install_requires=["selenium"],
     license="Apache License 2.0",
     classifiers=[
@@ -48,7 +48,7 @@ setup(
         "Environment :: Console",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
 )
